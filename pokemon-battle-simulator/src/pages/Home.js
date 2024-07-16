a04ccb9628b10bb5aa12cd5da0f77c6de0675a3e
choseongje <hangoy0606@gmail.com>
2024-07-16 15:44:05 +0900

    화면 전환 애니메이션 추가, css 파일 분리

--- pokemon-battle-simulator/src/pages/Home.js
+++ pokemon-battle-simulator/src/pages/Home.js
@@ -1,9 +1,18 @@
-import React from "react";
+// src/pages/Home.js
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../App.css";
+import "./Home.css";
 
 const Home = () => {
+  const [showAnimation, setShowAnimation] = useState(true);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowAnimation(false);
+    }, 2000); // 애니메이션 시간 (2초)
+    return () => clearTimeout(timer);
+  }, []);
 
   const handleStartGame = () => {
     navigate("/select-pokemon");
@@ -20,6 +29,7 @@ const Home = () => {
         <button onClick={handleStartGame}>게임 시작</button>
         <button onClick={handlePokedex}>포켓몬 도감</button>
       </div>
+      {showAnimation && <div className="pokeball-animation-close"></div>}
     </div>
   );
 };
